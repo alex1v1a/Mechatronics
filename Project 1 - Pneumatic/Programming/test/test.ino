d628981beeb53a55eb3e557a141c4f78a604e6ba
Alex <alex@1v1a.com>
2017-06-09 01:03:36 +1200

    start programming

---
+++ Project 1 - Pneumatic/Programming/test/test.ino
@@ -0,0 +1,34 @@
+#include <Wire.h>
+#include <VL6180X.h>
+
+VL6180X sensor;
+
+int butt = 8;
+int butts =0;
+int dist;
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  
+  sensor.init();
+  sensor.configureDefault();
+  sensor.setTimeout(500);
+  pinMode(2, OUTPUT);
+  pinMode(butt,INPUT);
+  
+}
+
+void loop() {
+  dist= sensor.readRangeSingle();
+ 
+  butts= digitalRead(butt);
+  if(butts ==1){
+    while(dist<=50){
+      digitalWrite(2,HIGH);
+      dist= sensor.readRangeSingle();
+    }
+  
+  }else if(butts ==0){
+    digitalWrite(2,LOW);
+  }
+}
